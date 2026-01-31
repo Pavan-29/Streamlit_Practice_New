36795956f4cea38cc1982d9945190f0b51b5d322
Pavan Shetty <pavanshetty2578@gmail.com>
2026-01-31 15:35:00 +0530

    Basic Streamlit

---
+++ intro.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Welcome to the MLOps Environment!")
+st.write("This is a simple Streamlit application running in your MLOps environment.")
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
